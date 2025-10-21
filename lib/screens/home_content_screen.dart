d890121dd28e426fcf3ca81d40bd599f80412cf3
LayanMDamrah <2layanmajed@gmail.com>
2025-10-22 02:02:21 +0300

    buttom nav bar

---
+++ lib/screens/home_content_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeContentScreen extends StatefulWidget {
+  const HomeContentScreen({super.key});
+
+  @override
+  State<HomeContentScreen> createState() => _HomeContentScreenState();
+}
+
+class _HomeContentScreenState extends State<HomeContentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text(' Home Page'));
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_gsg/screens/favorites_screen.dart';
+import 'package:recipe_gsg/screens/home_content_screen.dart';
 import 'package:recipe_gsg/services/shared_prefs.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomeContentScreen(),
+    const FavoritesScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text(_selectedIndex == 0 ? 'Home' : 'Favorites'),
+        centerTitle: true,
         actions: [
           IconButton(
             icon: const Icon(Icons.logout),
@@ -17,8 +39,19 @@ class HomeScreen extends StatelessWidget {
             },
           ),
         ],
-        title: const Text('Home'),
-        centerTitle: true,
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.orange,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+        ],
       ),
     );
   }
